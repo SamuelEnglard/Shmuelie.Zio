5d1ff950c160a5ff1260dc6339a25e576de2c96b
Shmueli Englard <shmueli.yosef@englard.net>
2021-02-23 21:39:54 -0500

    Added more tests and fixed bugs from tests

---
+++ Shmuelie.Zio.FileSystems.Tests/FlatFileSystemTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Zio;
+
+namespace Shmuelie.Zio.FileSystems.Tests
+{
+    [TestFixture]
+    [TestOf(typeof(FlatFileSystem))]
+    public class FlatFileSystemTests
+    {
+        private static readonly UPath[] paths = { "/a.txt", "/a/b.txt", "/a/c.text" };
+
+        private class TestFlatFS : FlatFileSystem
+        {
+            protected override IEnumerable<UPath> Paths { get; }
+
+            public TestFlatFS(IEnumerable<UPath> paths) => Paths = paths;
+
+            protected override UPath ConvertPathFromInternalImpl(string innerPath) => throw new NotImplementedException();
+            protected override string ConvertPathToInternalImpl(UPath path) => throw new NotImplementedException();
+            protected override void CopyFileImpl(UPath srcPath, UPath destPath, bool overwrite) => throw new NotImplementedException();
+            protected override void CreateDirectoryImpl(UPath path) => throw new NotImplementedException();
+            protected override void DeleteDirectoryImpl(UPath path, bool isRecursive) => throw new NotImplementedException();
+            protected override void DeleteFileImpl(UPath path) => throw new NotImplementedException();
+            protected override FileAttributes GetAttributesImpl(UPath path) => throw new NotImplementedException();
+            protected override DateTime GetCreationTimeImpl(UPath path) => throw new NotImplementedException();
+            protected override long GetFileLengthImpl(UPath path) => throw new NotImplementedException();
+            protected override DateTime GetLastAccessTimeImpl(UPath path) => throw new NotImplementedException();
+            protected override DateTime GetLastWriteTimeImpl(UPath path) => throw new NotImplementedException();
+            protected override void MoveDirectoryImpl(UPath srcPath, UPath destPath) => throw new NotImplementedException();
+            protected override void MoveFileImpl(UPath srcPath, UPath destPath) => throw new NotImplementedException();
+            protected override Stream OpenFileImpl(UPath path, FileMode mode, FileAccess access, FileShare share) => throw new NotImplementedException();
+            protected override void ReplaceFileImpl(UPath srcPath, UPath destPath, UPath destBackupPath, bool ignoreMetadataErrors) => throw new NotImplementedException();
+            protected override void SetAttributesImpl(UPath path, FileAttributes attributes) => throw new NotImplementedException();
+            protected override void SetCreationTimeImpl(UPath path, DateTime time) => throw new NotImplementedException();
+            protected override void SetLastAccessTimeImpl(UPath path, DateTime time) => throw new NotImplementedException();
+            protected override void SetLastWriteTimeImpl(UPath path, DateTime time) => throw new NotImplementedException();
+            protected override IFileSystemWatcher WatchImpl(UPath path) => throw new NotImplementedException();
+        }
+
+        [TestCaseSource(typeof(FlatFileSystemTests), nameof(DirectoryExistsTestData))]
+        public bool DirectoryExistsTest(UPath testPath) => new TestFlatFS(paths).DirectoryExists(testPath);
+
+        public static IEnumerable<TestCaseData> DirectoryExistsTestData()
+        {
+            yield return new TestCaseData(new UPath("/a")).Returns(true);
+            yield return new TestCaseData(new UPath("/b")).Returns(false);
+            yield return new TestCaseData(new UPath("/a/b")).Returns(false);
+            yield return new TestCaseData(new UPath("/a/b.txt")).Returns(false);
+            yield return new TestCaseData(UPath.Root).Returns(true);
+            yield return new TestCaseData(new UPath(null)).Returns(false);
+        }
+
+        [Test]
+        public void DirectoryExistsTest() => Assert.That(() => new TestFlatFS(paths).DirectoryExists(UPath.Empty), Throws.ArgumentException);
+
+        [TestCaseSource(typeof(FlatFileSystemTests), nameof(FileExistsTestData))]
+        public bool FileExistsTest(UPath testPath) => new TestFlatFS(paths).FileExists(testPath);
+
+        public static IEnumerable<TestCaseData> FileExistsTestData()
+        {
+            yield return new TestCaseData(new UPath("/a")).Returns(false);
+            yield return new TestCaseData(new UPath("/b")).Returns(false);
+            yield return new TestCaseData(new UPath("/a/c.text")).Returns(true);
+            yield return new TestCaseData(new UPath("/a/b.txt")).Returns(true);
+            yield return new TestCaseData(new UPath(null)).Returns(false);
+        }
+
+        [Test]
+        public void FileExistsTest() => Assert.That(() => new TestFlatFS(paths).FileExists(UPath.Empty), Throws.ArgumentException);
+
+        [TestCaseSource(typeof(FlatFileSystemTests), nameof(EnumeratePathsData))]
+        public int EnumeratePaths(UPath testPath, string searchPattern, SearchOption searchOption, SearchTarget searchTarget) => new TestFlatFS(paths).EnumeratePaths(testPath, searchPattern, searchOption, searchTarget).Count();
+
+        public static IEnumerable<TestCaseData> EnumeratePathsData()
+        {
+            yield return new TestCaseData(UPath.Root, "*.txt", SearchOption.AllDirectories, SearchTarget.File).Returns(2);
+            yield return new TestCaseData(UPath.Root, "*.txt", SearchOption.TopDirectoryOnly, SearchTarget.File).Returns(1);
+        }
+    }
+}
--- Shmuelie.Zio.FileSystems/FlatFileSystem.cs
+++ Shmuelie.Zio.FileSystems/FlatFileSystem.cs
@@ -19,18 +19,47 @@ namespace Shmuelie.Zio.FileSystems
         /// <value>
         ///     The list of paths in this file system.
         /// </value>
+        /// <remarks><para>Expected to only return files.</para></remarks>
         protected abstract IEnumerable<UPath> Paths
         {
             get;
         }
 
-        /// <inheritdoc />
-        protected sealed override bool FileExistsImpl(UPath path) => SearchPaths(path.GetDirectory().ToRelative(), path.GetName(), SearchOption.TopDirectoryOnly, SearchTarget.File).Any();
+        /// <summary>
+        ///     Determines whether the specified file exists.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns><see langword="true"/> if path contains the name of an existing file; otherwise, <see langword="false"/>.</returns>
+        /// <remarks><para>This method also returns <see langword="false"/> if path is <see langword="null"/>, an invalid path, or a zero-length string. If the caller does not have sufficient permissions to read the specified file, no exception is thrown and the method returns <see langword="false"/> regardless of the existence of path.</para></remarks>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is relative.</exception>
+        protected sealed override bool FileExistsImpl(UPath path)
+        {
+            path.AssertAbsolute();
+            return SearchPaths(path.GetDirectory(), path.GetName(), SearchOption.TopDirectoryOnly, SearchTarget.File).Any();
+        }
 
-        /// <inheritdoc />
-        protected sealed override bool DirectoryExistsImpl(UPath path) => SearchPaths(path.GetDirectory().ToRelative(), path.GetName(), SearchOption.TopDirectoryOnly, SearchTarget.Directory).Any();
+        /// <summary>
+        ///     Determines whether the given path refers to an existing directory on disk.
+        /// </summary>
+        /// <param name="path">The path to test.</param>
+        /// <returns><see langword="true"/> if the given path refers to an existing directory on disk, <see langword="false"/> otherwise.</returns>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is relative.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="path"/> is <see langword="null"/>.</exception>
+        protected sealed override bool DirectoryExistsImpl(UPath path)
+        {
+            path.AssertAbsolute();
+            return path == UPath.Root || SearchPaths(path.GetDirectory(), path.GetName(), SearchOption.TopDirectoryOnly, SearchTarget.Directory).Any();
+        }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Returns an enumerable collection of file names and/or directory names that match a search pattern in a specified path, and optionally searches subdirectories.
+        /// </summary>
+        /// <param name="path">The path to the directory to search.</param>
+        /// <param name="searchPattern">The search string to match against file-system entries in path. This parameter can contain a combination of valid literal path and wildcard (* and ?) characters (see Remarks), but doesn't support regular expressions.</param>
+        /// <param name="searchOption">One of the enumeration values that specifies whether the search operation should include only the current directory or should include all subdirectories.</param>
+        /// <param name="searchTarget">The search target either <see cref="SearchTarget.Both" /> or only <see cref="SearchTarget.Directory" /> or <see cref="SearchTarget.File" />.</param>
+        /// <returns>An enumerable collection of file-system paths in the directory specified by path and that match the specified search pattern, option and target.</returns>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is relative.</exception>
         /// <exception cref="ArgumentNullException"><paramref name="searchPattern"/> is <see langword="null" />.</exception>
         /// <exception cref="DirectoryNotFoundException"><paramref name="path"/> does not exist.</exception>
         protected sealed override IEnumerable<UPath> EnumeratePathsImpl(UPath path, string searchPattern, SearchOption searchOption, SearchTarget searchTarget)
@@ -48,6 +77,15 @@ namespace Shmuelie.Zio.FileSystems
             return SearchPaths(path, searchPattern, searchOption, searchTarget);
         }
 
+        /// <summary>
+        ///     Returns an enumerable collection of file names and/or directory names that match a search pattern in a specified path, and optionally searches subdirectories.
+        /// </summary>
+        /// <param name="path">The path to the directory to search.</param>
+        /// <param name="searchPattern">The search string to match against file-system entries in path. This parameter can contain a combination of valid literal path and wildcard (* and ?) characters (see Remarks), but doesn't support regular expressions.</param>
+        /// <param name="searchOption">One of the enumeration values that specifies whether the search operation should include only the current directory or should include all subdirectories.</param>
+        /// <param name="searchTarget">The search target either <see cref="SearchTarget.Both" /> or only <see cref="SearchTarget.Directory" /> or <see cref="SearchTarget.File" />.</param>
+        /// <returns>An enumerable collection of file-system paths in the directory specified by path and that match the specified search pattern, option and target.</returns>
+        /// <remarks><para>Differs from <see cref="EnumeratePathsImpl(UPath, string, SearchOption, SearchTarget)"/> by doing no checks.</para></remarks>
         private IEnumerable<UPath> SearchPaths(UPath path, string searchPattern, SearchOption searchOption, SearchTarget searchTarget)
         {
             SearchPattern search = SearchPattern.Parse(ref path, ref searchPattern);
@@ -55,17 +93,14 @@ namespace Shmuelie.Zio.FileSystems
 
             foreach (UPath resourceName in Paths)
             {
-                if (resourceName.IsInDirectory(path.ToRelative(), searchOption == SearchOption.AllDirectories))
+                if (resourceName.IsInDirectory(path, searchOption == SearchOption.AllDirectories) && (searchTarget == SearchTarget.Both || searchTarget == SearchTarget.File) && search.Match(resourceName))
                 {
-                    if ((searchTarget == SearchTarget.Both || searchTarget == SearchTarget.File) && search.Match(resourceName))
-                    {
-                        entries.Add(resourceName);
-                    }
-                    UPath resourceDirectory = resourceName.GetDirectory();
-                    if ((searchTarget == SearchTarget.Both || searchTarget == SearchTarget.Directory) && search.Match(resourceDirectory))
-                    {
-                        entries.Add(resourceDirectory);
-                    }
+                    entries.Add(resourceName);
+                }
+                UPath resourceDirectory = resourceName.GetDirectory();
+                if (resourceDirectory.IsInDirectory(path, searchOption == SearchOption.AllDirectories) && (searchTarget == SearchTarget.Both || searchTarget == SearchTarget.Directory) && search.Match(resourceDirectory))
+                {
+                    entries.Add(resourceDirectory);
                 }
             }
 
